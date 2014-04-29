e17732f4246ded37055a093ed97bba0d289ff44b
cele <celeweidmann@gmail.com>
2014-04-29 19:31:57 -0300

    propietario

---
+++ app/Controller/OwnersController.php
@@ -0,0 +1,16 @@
+<?php
+App::uses('AppController', 'Controller');
+/**
+ * Owners Controller
+ *
+ */
+class OwnersController extends AppController {
+
+/**
+ * Scaffold
+ *
+ * @var mixed
+ */
+	public $scaffold;
+
+}
